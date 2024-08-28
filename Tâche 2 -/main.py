056b2e5bb4d40cc5dc4b612d216fd0f830141545
CheikhDev <cheikhdev99@gmail.com>
2024-08-28 13:38:48 +0000

    premier commitpython

--- Tâche 2 -/main.py
+++ Tâche 2 -/main.py
@@ -1,1 +1,8 @@
-print("Hello Python")
+# Python
+x = int(input("Entrer un nombre : "))
+y = int(input("Entrer une puissance : "))
+
+def puissance(x,y):
+  return x**y
+
+print(f"{x} à la puissance {y} est egal à : {puissance(x,y)}")
